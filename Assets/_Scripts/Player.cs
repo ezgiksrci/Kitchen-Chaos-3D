ee1a5725ad6910587007051bcba3a1493679d805
ezgiksrci <keserci.ezgi@gmail.com>
2023-04-10 16:48:11 +0300

    Add animations, CM

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float turnSpeed = 20f;
+
+    private bool isWalking;
 
     private void Update()
     {
@@ -31,5 +34,18 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        if (isWalking)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, turnSpeed * Time.deltaTime);
+        }
+
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }

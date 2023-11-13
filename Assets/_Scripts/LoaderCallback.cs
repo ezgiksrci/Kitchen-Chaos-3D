fc7854df25af23825ef3e6b34414d620939dc534
Ezgi Keserci <keserci.ezgi@gmail.com>
2023-11-13 20:49:45 +0300

    Add main menu and loading scene

---
+++ Assets/_Scripts/LoaderCallback.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFirstFrame = true;
+
+    private void Update()
+    {
+        if (isFirstFrame)
+        {
+            isFirstFrame = false;
+
+            Loader.LoaderCallback();
+        }
+    }
+}
